37dc45706cb3e4faf8e1281950707d110d1194d1
mikeyuku <mikeyuko@gmail.com>
2022-04-08 16:47:55 +0800

    v0.2

--- flutter_begin_1/lib/home.dart
+++ flutter_begin_1/lib/home.dart
@@ -13,6 +13,37 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        leading:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.menu_rounded)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+        flexibleSpace: const SafeArea(
+          child: Icon(
+            Icons.photo_camera,
+            size: 75.0,
+            color: Colors.white70,
+          ),
+        ),
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.lightGreen.shade100,
+            height: 75.0,
+            width: double.infinity,
+            child: const Center(
+              child: Text('Bottom'),
+            ),
+          ),
+          preferredSize: const Size.fromHeight(75.0),
+        ),
+        shadowColor: Colors.red,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -35,7 +66,33 @@ class ContainerWithDecorationWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(), /////// to be continue
+        Container(
+          height: 100.0,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50.0),
+              bottomRight: Radius.circular(10.0),
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white,
+                Colors.lightGreen,
+              ],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: const Center(
+            child: Text('Container'),
+          ),
+        ),
       ],
     );
   }
